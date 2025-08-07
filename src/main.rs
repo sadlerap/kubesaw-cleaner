e0dd374f4e22ef0a0059ca6273dbd966febb6ab3
Andy Sadler <ansadler@redhat.com>
2025-08-07 08:31:08 -0500

    remove crds and known webhooks
    In order to successfully clean the clusters, we need to remove a webhook
    configuration that was not managed by argo nor by olm that's preventing
    removal of spacebindingrequests.  Once it's been removed, we can safely
    delete the CRDs once we've patched the finalizers.
    
    Signed-off-by: Andy Sadler <ansadler@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-#![allow(dead_code)]
-use color_eyre::eyre::eyre;
+use color_eyre::eyre::{eyre, Context};
 use itertools::Itertools;
-use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinition;
+use k8s_openapi::{
+    api::admissionregistration::v1::ValidatingWebhookConfiguration,
+    apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinition,
+};
 use kube::{
     api::{ApiResource, DynamicObject, GroupVersionKind, ListParams},
     core::Expression,
-    Api, Client, Resource,
+    Api, Client, Resource, ResourceExt,
 };
-use tracing::{error, info, instrument, level_filters::LevelFilter};
+use tracing::{error, info, instrument, level_filters::LevelFilter, trace};
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 #[tokio::main]
@@ -39,7 +41,13 @@ fn init_tracing() -> color_eyre::Result<()> {
 async fn run() -> color_eyre::Result<()> {
     let client = Client::try_default().await?;
 
-    // step 1: get all kubesaw crds
+    // step 1: remove the "users.spacebindingsrequests.webhook.sandbox" validating webhook config,
+    // since its existance blocks patching spacebindingrequests
+    remove_webhook_configs(&client)
+        .await
+        .wrap_err("failed to remove stale webhooks, bailing")?;
+
+    // step 2: get all kubesaw crds
     // they should have the OLM labels set, so we can use them
     let crds: Api<CustomResourceDefinition> = Api::all(client.clone());
     let host_expr = Expression::Exists(
@@ -60,27 +68,63 @@ async fn run() -> color_eyre::Result<()> {
         .unique_by(|crd| &crd.metadata.name)
     {
         info!(name = crd.metadata.name, "updating crds");
-        let _ = patch_crs(&client, crd).await.inspect_err(|_| {
-            error!("Failed to update CRs for resource {:?}", crd.metadata.name);
+        // step 3: patch all instances of our custom resources to remove their finalizer
+        let _ = patch_crs(&client, crd).await.inspect_err(|e| {
+            error!(
+                "Failed to update CRs for resource {:?}",
+                crd.metadata.name.as_deref().unwrap_or("")
+            );
+            eprint!("{e}");
         });
+
+        // step 4: remove the crd itself
+        remove_crd(&client, crd)
+            .await
+            .wrap_err_with(|| format!("failed to delete crd {}", crd.name_any()))?;
     }
 
     Ok(())
 }
 
+async fn remove_webhook_configs(client: &Client) -> color_eyre::Result<()> {
+    let webhooks: Api<ValidatingWebhookConfiguration> = Api::all(client.clone());
+    webhooks
+        .delete(
+            "users.spacebindingrequests.webhook.sandbox",
+            &Default::default(),
+        )
+        .await?
+        .map_left(|_| info!("deleting webhooks"))
+        .map_right(|_| info!("deleted webhook config"));
+
+    Ok(())
+}
+
+#[instrument(skip_all, fields(crd_name = crd.name_any()))]
+async fn remove_crd(client: &Client, crd: &CustomResourceDefinition) -> color_eyre::Result<()> {
+    let crds: Api<CustomResourceDefinition> = Api::all(client.clone());
+    if let Some(name) = crd.metadata.name.as_deref() {
+        crds.delete(name, &Default::default())
+            .await?
+            .map_left(|_| info!("deleting crd"))
+            .map_right(|_| info!("deleted crd"));
+    }
+    Ok(())
+}
+
 /// For a given custom resource definition, remove the finalizer for all instances
-#[instrument(skip_all, fields(name = crd.metadata.name))]
+#[instrument(skip_all, fields(crd_name = crd.name_any()))]
 async fn patch_crs(client: &Client, crd: &CustomResourceDefinition) -> color_eyre::Result<()> {
     let version = crd
         .spec
         .versions
         .iter()
-        .find(|v| v.served)
+        .find(|v| v.storage)
         .map(|v| v.name.clone())
         .ok_or_else(|| {
             error!(
                 name = crd.meta().name,
-                "failed to find served version of crd"
+                "failed to find storage version of crd"
             );
             color_eyre::eyre::eyre!("CRD parsing failed")
         })?;
@@ -88,7 +132,7 @@ async fn patch_crs(client: &Client, crd: &CustomResourceDefinition) -> color_eyr
         &GroupVersionKind::gvk(&crd.spec.group, &version, &crd.spec.names.kind),
         &crd.spec.names.plural,
     );
-    info!(
+    trace!(
         version = dyntype.version,
         group = dyntype.group,
         kind = dyntype.kind,
@@ -126,12 +170,12 @@ async fn patch_crs(client: &Client, crd: &CustomResourceDefinition) -> color_eyr
 
             object.metadata.finalizers = Some(new_finalizers);
             info!(name, namespace, "patching custom resource");
-            api.replace(name, &Default::default(), &object)
+            let _ = api
+                .replace(name, &Default::default(), &object)
                 .await
-                .map_err(|err| {
+                .inspect_err(|err| {
                     error!(?err, namespace, name, "failed to update finalizers");
-                    eyre!("failed to update finalizers: {:?}", err)
-                })?;
+                });
         }
     }
 
